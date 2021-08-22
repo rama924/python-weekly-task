cea6edd4330ff74772d315143f98b85546ea257d
rama924 <88873873+rama924@users.noreply.github.com>
2021-08-22 20:11:11 +0530

    Add files via upload

---
+++ Week+1+task+.py
@@ -0,0 +1,123 @@
+
+# coding: utf-8
+
+# In[1]:
+
+# 1. Create a variable named carname and assign the value Volvo to it.
+carname = 'Volvo'
+print(carname)
+
+
+# In[2]:
+
+# 2)Multiply 10 with 5, and print the result.
+a=5*10
+print(a)
+
+
+# In[5]:
+
+#3)what will be the output of print 9//2 and why? Funcation // will be used to get quotient of the result thatis the reason its retun 4.
+9//2
+
+
+# In[6]:
+
+# 4)Create a string, an integer, and a floating point number. 
+# The string should be named mystring and should contain the word "hello". 
+#The floating point number should be named myfloat and should contain the number 10.0, and the integer should be named myint and should contain the number 20.
+mystring = 'hello'
+myfloat = '10.0'
+myint = '20'
+print(mystring,myfloat,myint)
+
+
+# In[7]:
+
+# 5)What is the output of the following assignment operator
+y = 10
+x =y +2
+print(x)
+
+
+# In[8]:
+
+#6)What is the output of the following code
+x = 100
+y = 50
+print(x * y)
+
+
+# In[10]:
+
+#7)What is the output of print(2%6)
+print(2%6)
+
+
+# In[54]:
+
+#8)How would you check if each word in a string begins with a capital letter?
+mystring = "Hello"
+mystring.isupper()
+
+
+# In[14]:
+
+#9) Count the total number of characters in a string
+# This can be achived by using len().
+x = "This is Rama"
+len(x)
+
+
+# In[73]:
+
+#10) Split a string on a specific character(Use space as a delimiter in any string)
+python = "This is rama. I am learningPython"
+len(python)
+python[8:12]
+
+
+# In[55]:
+
+# 11)Uppercase or lowercase an entire string.
+cars =["Audi", "BMW", "Ford"]
+maintaince = "We need to maintain {}, {} and {}"
+maintaince.format(cars[0].upper(), cars[1].lower(), cars[2])
+
+
+# In[59]:
+
+# 12)What is the effect of multiplying a string by 3?
+# Ans string will print 3 times example:
+learn = "God is great"
+learn1 = learn*3 
+print(learn1)
+
+
+# In[60]:
+
+# 13)Concatenate two strings
+a = "Plant the trees"
+b = " Trees will give oxigen"
+c = a+b
+print(c)
+
+
+# In[71]:
+
+# 14)prints the characters of string from 3 to 7 skipping one character.
+gopi = "World is great!"
+print(gopi[3:7])
+
+
+# In[64]:
+
+# 15)what is the output of print "Hello World"[::-1] and why?
+# :: will repeat the slicing in python & [::-1] returns the reverse of the string.
+print("Hello world!"[::-1])
+
+
+# In[ ]:
+
+
+
